a411ef7b7341df442aed9cd17bbafe9e71a4141a
saiiirus <cyrus@elogicinnovations.com>
2024-03-18 15:09:32 +0800

    test

--- backend/db/config/sequelize.config.js
+++ backend/db/config/sequelize.config.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   host: "localhost",
-  database: "erp_sbf",
+  database: "eli_pos",
   dialect: "mysql",
   username: "root",
   password: "",
